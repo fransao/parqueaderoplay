f084b160bce183e5ccb2d1119e82c66251f8fe07
fransao <francisco-salazar8a@hotmail.com>
2018-07-24 16:59:51 -0500

    consumir post, get

--- app/filters/ExampleFilter.scala
+++ app/filters/ExampleFilter.scala
@@ -14,6 +14,7 @@ import scala.concurrent.ExecutionContext
  */
 @Singleton
 class ExampleFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFilter {
+
   override def apply(next: EssentialAction) = EssentialAction { request =>
     next(request).map { result =>
       result.withHeaders("X-ExampleFilter" -> "foo")
